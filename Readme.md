444d34f968ee2806f2adf1805018f02379c12b8d
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-10-15 11:58:28 +0530

    readme added

---
+++ Readme.md
@@ -0,0 +1,28 @@
+### About
+
+It is a desktop application built for showcasing a simple Chat-Bot which is trained on simple Knowledge Base.
+We've used the following Tech Stack:
+
+- UI: Tkinter,
+- Preprocessing: NLTK, Numpy,
+- ML Algorithm: Simple ANN,
+
+### How to use
+
+Install all the dependencies using:
+
+First go to terminal:
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python app.py
+```
+
+Some previews
+
+![s1](./images/s1.png)
+
+![s2](./images/s2.png)
--- chat.py
+++ chat.py
@@ -6,9 +6,9 @@ import torch
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-with open('intents.json', 'r') as json_data:
+with open("intents.json", "r") as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
@@ -17,8 +17,8 @@ data = torch.load(FILE)
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
+all_words = data["all_words"]
+tags = data["tags"]
 model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
@@ -26,7 +26,7 @@ model.load_state_dict(model_state)
 model.eval()
 
 #### FOR TERMINAL
-# bot_name = "Jarvis"
+bot_name = "Jarvis"
 # print("Let's chat! (type 'quit' to exit)")
 # while True:
 #     # sentence = "do you use credit cards?"
@@ -53,6 +53,7 @@ model.eval()
 #     else:
 #         print(f"{bot_name}: I do not understand...")
 
+
 def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
@@ -67,8 +68,8 @@ def get_response(msg):
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
-        for intent in intents['intents']:
+        for intent in intents["intents"]:
             if tag == intent["tag"]:
-                return random.choice(intent['responses'])
-    
+                return random.choice(intent["responses"])
+
     return "I do not understand..."
